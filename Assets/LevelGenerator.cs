8a123057613c6e24ce5e13e5cee96b158129ab4b
Mitch <ddiarikov@gmail.com>
2021-04-24 22:25:26 +0300

    deleted ObjectPool.cs, LevelGenerator.cs

--- Assets/LevelGenerator.cs
+++ Assets/LevelGenerator.cs
@@ -2,21 +2,34 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-    [SerializeField] ObjectPool[] objectPools;
     [SerializeField] int worldSize = 10;
-    [SerializeField] float tileHeight = 9f;
+    [SerializeField] float tileHeight = 1f;
+    [SerializeField] int poolSize;
+    [SerializeField] GameObject objectPrefab;
+
+    GameObject[] activePool;
 
     GameObject player;
+
+    private void Awake()
+    {
+        activePool = PopulatePool();
+    }
 
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         
         //Sets up the initial tiles in the world.
-        for (int i = -5; i < worldSize - 5; i++)
+        for (int i = - Mathf.RoundToInt(worldSize/2); i < Mathf.RoundToInt(worldSize / 2); i++)
         {
             GenerateTile(tileHeight * i);
         }
+    }
+
+    private void Update()
+    {
+        
     }
 
     float DistanceToPlayer()
@@ -25,33 +38,46 @@ public class LevelGenerator : MonoBehaviour
         return distanceToPlayer;
     }
 
-    public void DisableTile(GameObject obj)
-    {
-        //Return the tile to the pool
-        obj.SetActive(false);
-
-        //Create a new replacement tile for the far end of the level
-        //GenerateTile(tileDepth * worldSize + (tileDespawner.transform.position.z + (2 * tileDepth)));
-    }
-
     void GenerateTile(float yOffset)
     {
         //Select a tile from a random pool
-        int selectedPool = Random.Range(0, objectPools.Length);
+        int selectedPool = Random.Range(0, activePool.Length);
 
-        GameObject poolObject = objectPools[selectedPool].EnableObjectInPool(0, yOffset, 0);
+        GameObject poolObject = EnableObjectInPool(0, yOffset, 0);
 
-     /*   //If there was not availalbe tile in the pool, naively select the first one you can find from another pool
         if (poolObject == null)
         {
-            foreach (ObjectPool pool in objectPools)
-            {                
-                poolObject = pool.EnableObjectInPool(yOffset);
+            Debug.LogError("[LevelGenerator] GenerateTile poolObject == null");
+        }    
+    }
 
-                float yOffset +
+    GameObject[] PopulatePool()
+    {
+        //Create the pool
+        activePool = new GameObject[poolSize];
 
-                if (poolObject != null) { break; }
+        //Instantiate the objects into the pool and disable them
+        for (int i = 0; i < activePool.Length; i++)
+        {
+            activePool[i] = Instantiate(objectPrefab, transform);
+            activePool[i].SetActive(false);
+        }
+
+        return activePool;
+    }
+
+    public GameObject EnableObjectInPool(float xOffset, float yOffset, float zOffset)
+    {
+        //Find the next available object in the pool and try to enable it
+        for (int i = 0; i < activePool.Length; i++)
+        {
+            if (!activePool[i].activeInHierarchy)
+            {
+                activePool[i].transform.position = new Vector3(transform.position.x + xOffset, transform.position.y + yOffset, transform.position.z + zOffset);
+                activePool[i].SetActive(true);
+                return activePool[i];
             }
-        } */
+        }
+        return null;
     }
 }
